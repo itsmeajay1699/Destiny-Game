8238708756a3554ac22b94df0978d74fd38f3954
Ajay <ajayroy03377@gmail.com>
2022-10-19 16:23:27 +0530

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story{
+  String storytale;
+  String choice;
+  String choice3;
+  Story({String storyTitle, String choice1, String choice2}){
+    storytale =storyTitle;
+    choice=choice1;
+    choice3=choice2;
+  }
+}
